7149dfb6423e01efc9c72a10cd57d76d98bb25ba
KregTM <75927089+IchiboLB@users.noreply.github.com>
2022-03-13 15:27:55 +0600

    Initial commit

---
+++ app/src/main/java/com/cornbeer/omogallery/startFragment/StartFragment.kt
@@ -0,0 +1,29 @@
+package com.cornbeer.omogallery.startFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.cornbeer.omogallery.R
+
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val headspaceButton = view.findViewById<Button>(R.id.headspace)
+        headspaceButton.setOnClickListener {
+            println("Dobriden")
+        }
+    }
+}
