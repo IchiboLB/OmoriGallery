cfcda3b7b88e7d2a8ca118599f89097aaaafb997
IchiboLB <75927089+IchiboLB@users.noreply.github.com>
2022-03-27 10:54:36 +0600

    third screen
    AUBREY

---
+++ app/src/main/java/com/cornbeer/omogallery/aubreyFragment/AubreyFragment.kt
@@ -0,0 +1,24 @@
+package com.cornbeer.omogallery.aubreyFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.cornbeer.omogallery.R
+
+class AubreyFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_aubreymelon, container, false)
+    }
+
+
+
+}
--- app/src/main/java/com/cornbeer/omogallery/headchoiceFragment/HeadchoiceFragment.kt
+++ app/src/main/java/com/cornbeer/omogallery/headchoiceFragment/HeadchoiceFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.cornbeer.omogallery.R
 
 class HeadchoiceFragment : Fragment() {
@@ -14,5 +16,14 @@ class HeadchoiceFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_firsthead, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val aubreyButton = view.findViewById<Button>(R.id.aubrey)
+        aubreyButton.setOnClickListener {
+            val navhost = findNavController()
+            navhost.navigate(R.id.action_headchoiceFragment_to_aubreyFragment)
+        }
     }
 }
